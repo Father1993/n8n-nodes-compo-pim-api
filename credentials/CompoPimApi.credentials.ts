e815b152ffe9ae672eb8287969ced2e57d5fa4b0
Father1993 <enjoy_hill@gmail.com>
2025-09-03 15:12:45 +1000

    fix: bugs with ai

--- credentials/CompoPimApi.credentials.ts
+++ credentials/CompoPimApi.credentials.ts
@@ -5,6 +5,14 @@ export class CompoPimApi implements ICredentialType {
     displayName = 'Compo PIM API'
     properties = [
         {
+            displayName: 'Server URL',
+            name: 'serverUrl',
+            type: 'string' as NodePropertyTypes,
+            default: 'https://pim.uroven.pro',
+            placeholder: 'https://pim.uroven.pro',
+            description: 'Base URL of your Compo PIM server',
+        },
+        {
             displayName: 'Login',
             name: 'login',
             type: 'string' as NodePropertyTypes,
